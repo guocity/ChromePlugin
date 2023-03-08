b02295f0cae4297a9b6851038e5cd905387cfd18
guocity <guocity@users.noreply.github.com>
2023-03-07 22:14:17 -0500

    update to version 3
    update to version 3

--- plugin/action.js
+++ plugin/action.js
@@ -1,15 +1,29 @@
 //when i click on my button
 document.getElementById('fillForm').addEventListener('click', function(){
     //grab the text filed value from my tool
-    var intValue = document.getElementById('apivalue').value;
 
-    chrome.tabs.executeScript({
-        //send the value to be used by our script
-        code: `var value = ${intValue};`
-    }, function() {
-        //run the script in the file injector.js
-        chrome.tabs.executeScript({
-            file: 'injector.js'
-        });
-    });
+
+    var formRead = document.getElementsByTagName("form");
+
+    // Print "form readed" on the page
+        document.write("form readed");
+        document.write(formRead.innerHTML);
+        alert("here");
+
+
+
+
+
+
+    // var intValue = document.getElementById('apivalue').value;
+
+    // chrome.tabs.executeScript({
+    //     //send the value to be used by our script
+    //     code: `var value = ${intValue};`
+    // }, function() {
+    //     //run the script in the file injector.js
+    //     chrome.tabs.executeScript({
+    //         file: 'injector.js'
+    //     });
+    // });
   });
